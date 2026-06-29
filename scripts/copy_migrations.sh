eac4ab6da04ac05794f03c849cc90d5d1d1f8f41
Shashankcj <shashankcj7@gmail.com>
2026-06-29 17:15:46 +0530

    update folders 1

--- scripts/copy_migrations.sh
+++ scripts/copy_migrations.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e
 source venv/bin/activate
+echo "Running collectstatic..."
+yes yes | python manage.py collectstatic
+
+echo "Finding Django path..."
+DJANGO_BASE_PATH=$(python -c "import django; print(django.__path__[0])")
+
+echo "Django path: $DJANGO_BASE_PATH"
+
+if [ ! -d "$DJANGO_BASE_PATH/contrib/admin/templates" ]; then
+    echo "Django admin templates not found!"
+    exit 1
+fi
+
 cd src/backend
 
 VENV=$(python -c "import site; print(site.getsitepackages()[0])")
